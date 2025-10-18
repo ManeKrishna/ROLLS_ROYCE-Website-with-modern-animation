0d03bca9b51220ef217e970b9985ce0396804210
Krishna Mane <kdm12899@gmail.com>
2025-10-18 17:01:42 +0530

    Deployment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import Video from '../components/home/video'
+
 import HomeHeroText from '../components/home/HomeHeroText'
 import HomeBottomText from '../components/home/HomeBottomText'
+import Video from '../components/home/video'
 
 const Home = () => {
   return (
     <div >
       <div className=' h-screen w-screen fixed'>
-        <Video />
+        <Video/>
       </div>
       <div className='h-screen w-screen relative flex flex-col justify-between'> 
         <HomeHeroText/>
